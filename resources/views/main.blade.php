01112a71f5615f5295a3f138810d023c339ad5ee
Tong Gan <gantong1986@gmail.com>
2017-11-15 22:09:08 +0000

    Use Sass and compile bulma for better css

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,1 +1,13 @@
 
+@extends('app')
+
+@section('content')
+<ul>
+  @foreach($staffs as $staff)
+  <li>{{ $staff->workhours }}</li>
+  @endforeach
+</ul>
+
+<a class="button is-small is-primary">Small</a>
+
+@endsection
